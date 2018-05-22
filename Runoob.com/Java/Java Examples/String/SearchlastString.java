22cc530e3eabdfcfd3ee76e7b5f530d7ad8d47c7
SongTeng <songteng0604@outlook.com>
2018-05-22 19:55:36 +0800

    Add files via upload

---
+++ Runoob.com/Java/Java Examples/String/SearchlastString.java
@@ -0,0 +1,17 @@
+/** 字符串函数strOrig.lastIndexOf(Stringname)来查找字符串Stringname 在 strOrig中出现的位置 */
+
+
+package Examples.String;
+
+public class SearchlastString {
+    public static void main(String[] args){
+        String strOrig = "Hello world,Hello Runoob";
+        int lastIndex = strOrig.lastIndexOf("Runoob");
+        if(lastIndex == -1){
+            System.out.println("没找到字符串Runoob");
+        }else{
+            System.out.println("Runoob字符串最后出现的位置索引为："+lastIndex);
+        }
+
+    }
+}
