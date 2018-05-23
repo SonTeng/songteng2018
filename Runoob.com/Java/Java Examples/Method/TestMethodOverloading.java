a9089dc97ca774dfc12a719fb0f9af3e574b08ce
SongTeng <songteng0604@outlook.com>
2018-05-23 19:59:41 +0800

    Create TestMethodOverloading.java

---
+++ Runoob.com/Java/Java Examples/Method/TestMethodOverloading.java
@@ -0,0 +1,32 @@
+public class TestMethodOverloading {
+    public static void main(String[] args){
+        MyClass t = new MyClass(3);
+        t.info();
+        t.info("重载方法");
+        new MyClass();
+    }
+}
+
+class MyClass{
+    int height;
+    MyClass(){
+        System.out.println("无参数构造函数");
+        height=4;
+    }
+    MyClass(int i){
+        System.out.println("房子高度为 "+i+" 米");
+        height=i;
+    }
+    void info(){
+        System.out.println("房子高度为 "+height+" 米");
+    }
+    void info(String s){
+        System.out.println(s+":房子高度为 "+height+" 米");
+    }
+}
+------------------------
+Output:
+房子高度为 3 米
+房子高度为 3 米
+重载方法:房子高度为 3 米
+无参数构造函数
