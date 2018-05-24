8b0447fb6cbb7168f860583ffa35743e995d6830
SongTeng <songteng0604@outlook.com>
2018-05-24 14:19:56 +0800

    Create Rect.java

---
+++ Runoob.com/Java/Java Examples/Tuxing/Rect.java
@@ -0,0 +1,20 @@
+package Examples.Tuxing;
+
+public class Rect {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("*");
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+-------------
+Output:
+******
+******
+******
+******
+******
