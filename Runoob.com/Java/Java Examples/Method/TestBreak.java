e882eeb3ab3cc3b196ef18dbfe94e5b979068a73
SongTeng <songteng0604@outlook.com>
2018-05-24 13:58:48 +0800

    Create TestBreak.java

---
+++ Runoob.com/Java/Java Examples/Method/TestBreak.java
@@ -0,0 +1,25 @@
+/**Java 中break语句可以直接强行退出当前的循环，忽略循环体中任何其他语句的循环条件测试。*/
+
+public class TestBreak {
+    public static void main(String[] args){
+        int[] intary = {99,12,22,34,45,67,5678,8990};
+        int no =5678;
+        int i=0;
+        boolean found=false;
+        for(;i<intary.length;i++){
+            if(intary[i]==no){
+                found=true;
+                break;
+            }
+        }
+        if(found){
+            System.out.println(no+"元素的索引位置在："+i);
+        }else{
+            System.out.println(no+"元素不在数组中");
+        }
+    }
+}
+
+-------------
+Output:
+5678元素的索引位置在：6
