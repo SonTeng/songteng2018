f424960bb2548bb5450d172116f902e273f33baa
SongTeng <songteng0604@outlook.com>
2018-05-30 22:27:52 +0800

    Create Main.java

---
+++ DesignPatterns/Adapter/ObjectAdapter/Main.java
@@ -0,0 +1,9 @@
+package Adapter.Object;
+
+public class Main {
+    public static void main(String[] args){
+        Print p=new PrintBanner("Welcome");
+        p.printWeak();
+        p.printStrong();
+    }
+}
